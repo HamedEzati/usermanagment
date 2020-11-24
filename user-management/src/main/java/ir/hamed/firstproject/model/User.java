1b31d89e46eea4f783666683b861e4024dff9537
HamedEzati <68893075+HamedEzati@users.noreply.github.com>
2020-11-25 00:47:14 +0330

    Add files via upload

---
+++ user-management/src/main/java/ir/hamed/firstproject/model/User.java
@@ -0,0 +1,55 @@
+package ir.hamed.firstproject.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+
+    private String name;
+    private String family;
+    @Id
+    private String username;
+    private String email;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
